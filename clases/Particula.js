bd33a2604d44d7aec4f79d2d482a48a27c7a6556
JimmyCauja <jimmycauja@gmail.com>
2023-11-09 16:14:38 -0500

    Se crearon varios sitemas de particulas

--- clases/Particula.js
+++ clases/Particula.js
@@ -1,23 +1,23 @@
 class Particula {
     constructor(_x, _y) {
-        this.pos =
-            createVector(_x, _y);
+        this.pos = createVector(_x, _y);
         this.lifespan = Math.ceil(random(10, 200));
         this.isAlive = true; 
-        this.side = Math.cell(random(3, 15));
+        this.side = Math.ceil(random(3, 15));
+        this.color = random(0, 150);
     }
     update() {
         this.r = random(100);  
         
-        if (this.r < 25) {
+        if (this.r < 20) {
             //hacia arriba
             this.pos.y -= this.side;
             //hacia la derecha
-        } else if (this.r < 50) {
+        } else if (this.r < 40) {
             this.pos.x += this.side;
             //hacia abajo
         } else if (this.r < 75) {
-            this.pos.x += this.side;
+            this.pos.y += this.side;
         } else {
             //hacia izquierda
             this.pos.x -= this.side;  
@@ -32,6 +32,8 @@ class Particula {
         }
     }
     display() {
+        fill(this.color);
+        stroke(255);
         square(this.pos.x, this.pos.y, this.side);
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
+let s;
+let v;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+	s = new Sistema();
+	v = new Sistema();
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+	background(10, 20,10);
+	s.update();
+	s.display();
+	v.update();
+	v.display();
 }
