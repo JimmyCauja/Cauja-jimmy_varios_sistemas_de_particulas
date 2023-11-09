5acebfe297b9893cdd7712ca254b748b353efeb8
JimmyCauja <jimmycauja@gmail.com>
2023-11-09 15:09:32 -0500

    se creo una actulaizacion

--- clases/Particula.js
+++ clases/Particula.js
@@ -3,12 +3,33 @@ class Particula {
         this.pos =
             createVector(_x, _y);
         this.lifespan = Math.ceil(random(10, 200));
-        this.isDead = true; 
+        this.isAlive = true; 
         this.side = Math.cell(random(3, 15));
     }
     update() {
-        this.r = random();
+        this.r = random(100);  
         
+        if (this.r < 25) {
+            //hacia arriba
+            this.pos.y -= this.side;
+            //hacia la derecha
+        } else if (this.r < 50) {
+            this.pos.x += this.side;
+            //hacia abajo
+        } else if (this.r < 75) {
+            this.pos.x += this.side;
+        } else {
+            //hacia izquierda
+            this.pos.x -= this.side;  
+        }
+        
+        this.lifespan -= 1;
+
+        if (this.lifespan <= 0) {
+            this.isAlive = false;
+
+            
+        }
     }
     display() {
         square(this.pos.x, this.pos.y, this.side);
